efd7b636b7c75af33e3b48385b1e817394907399
john <johnkimdw@gmail.com>
2022-06-08 18:04:37 +0800

    updated frontend

---
+++ backend/main.js
@@ -0,0 +1,6 @@
+function search_kdrama() {
+  let input = document.getElementById('userInput').value;
+  input = input.toLowerCase();
+  // this is where the python should come in or smth (see re_sys.py)
+  // search for matching kdramas:
+}
--- recommender/rec_sys.py
+++ recommender/rec_sys.py
@@ -3,27 +3,39 @@ from sklearn.metrics.pairwise import cosine_similarity # finds similarity betwee
 
 import pandas as pd # dataframe library
 
-filename = "csv\shows.csv"
+filename = r"csv\kdrama_data.csv"
 
 df = pd.read_csv(filename)
-df = df[['Name', 'Rank', 'Synopsis']]
+
+df = df[['title', 'description', 'keywords', 'genres', 'actors', 'director', 'screenwriter']]
+
+def fill_na():
+    """replaces all na values with an empty string to prevent error"""
+    df.replace("N/A", "")
+    for label in df.columns:
+        df[label] = df[label].fillna('') # fills N/A values with ""
+
+fill_na()
 
 tfidf = TfidfVectorizer(stop_words='english')
-df['Synopsis'] = df['Synopsis'].fillna('') # null synposis = ""
+tfidf_matrix = tfidf.fit_transform(df['keywords'] + " " + df['genres']
+ + " " + df['actors'] + " " + df['director'] + " " + df['screenwriter'])
 
-tfidf_matrix = tfidf.fit_transform(df['Synopsis'])
-tfidf_matrix.shape # (100, 1853) means 100 movies, 1853 unique words
+# uses cosine to find the angle between the vector to find level of similarity
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix) # calculates similarity between each drama (?)
-cosine_sim[0][1] # compares similarity between number 1 movie with number 2 movie
-
-indices = pd.Series(df.index, index=df['Name'])
+# indices of kdramas (e.g. top rank = 0 = move to heaven)
+indices = pd.Series(df.index, index=df['title'])
 indices = indices[~indices.index.duplicated(keep='last')]
 
 def search_kdrama(kdrama_name, kdrama_indices):
+    """searches kdrama for matching names """
     return kdrama_indices[kdrama_indices.index.str.contains(kdrama_name, na=False)]
 
 def get_recommended_kdramas(target_kdrama_index, kdrama_similarities, kdramas_df):
     similarity_scores = pd.DataFrame(kdrama_similarities[target_kdrama_index], columns=["score"])
-    kdrama_indices = similarity_scores.sort_values("score", ascending=False)[0:11].index
-    return df['Name'].iloc[kdrama_indices]
+    kdrama_indices = similarity_scores.sort_values("score", ascending=False)[1:11].index
+    return pd.concat([kdramas_df['title'].iloc[kdrama_indices], similarity_scores.iloc[kdrama_indices]], axis = 1)
+
+get_recommended_kdramas(1, cosine_sim, df)
+search_kdrama("Move", indices)
